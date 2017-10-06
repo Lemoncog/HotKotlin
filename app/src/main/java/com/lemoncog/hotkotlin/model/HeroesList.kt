98dbb2980b53003f33553a96e0f830af0bab6e8f
Jason Leeming - Mobile iPlayer <Jason.Leeming@bbc.co.uk>
2017-10-06 14:01:58 +0100

    Adding in concept of plays/wins/losses

--- app/src/main/java/com/lemoncog/hotkotlin/model/HeroesList.kt
+++ app/src/main/java/com/lemoncog/hotkotlin/model/HeroesList.kt
@@ -1,5 +1,5 @@
 package com.lemoncog.hotkotlin.model
 
-data class Hero(val name: String, val subGroup: String, val group : Group)
+data class Hero(val id: Long, val name: String, val subGroup: String, val group : Group, val played: Int = 0, val wins: Int = 0, val losses : Int = 0)
 
 data class HeroesList(val heroesList: List<Hero>)
--- app/src/main/java/com/lemoncog/hotkotlin/service/model/HeroesListProvider.kt
+++ app/src/main/java/com/lemoncog/hotkotlin/service/model/HeroesListProvider.kt
@@ -7,6 +7,7 @@ import com.lemoncog.hotkotlin.model.fromString
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONArray
+import java.util.*
 
 
 class HeroesListProvider {
@@ -24,21 +25,15 @@ class HeroesListProvider {
 
             val heroesListArray: MutableList<Hero> = mutableListOf<Hero>()
 
-//            for (i in 0..jsonList.length()) {
-//                val heroObj = jsonList.getJSONObject(i)
-//
-//                val primaryName = heroObj.getString("PrimaryName");
-//
-//                heroesListArray.add(Hero(primaryName))
-//            }
-
+            var index = 0L
+            val rand = Random()
 
             (0..jsonList.length()-1)
                     .asSequence()
                     .map { jsonList.getJSONObject(it) }
                     .mapTo(heroesListArray)
                     {
-                        Hero(it.getString("PrimaryName"), it.getString("SubGroup"), fromString(it.getString("Group")))
+                        Hero(index++, it.getString("PrimaryName"), it.getString("SubGroup"), fromString(it.getString("Group")), played = rand.nextInt(10), wins = rand.nextInt(5), losses = rand.nextInt(5))
                     }
 
 
